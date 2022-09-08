7cd566841fff128d93b06fe72ebbbfe4a173201a
Devsinc <dev@Devsincs-MacBook-Pro.local>
2022-09-09 02:14:02 +0500

    Implemented bootstrap styling on posts, suggestions, comment, replies

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
+# UserPolicy
 class UserPolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
     # def resolve
     #   scope.all
     # end
-
   end
 end
