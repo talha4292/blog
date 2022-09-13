ee6aa1094d3e6d88034e143d887fecf7b4f1e69a
Devsinc <dev@Devsincs-MacBook-Pro.local>
2022-09-13 17:45:51 +0500

    email configs for production changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,7 +6,7 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
-  config.action_mailer.default_url_options = { host: 'rails-bloggy.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://rails-bloggy.herokuapp.com' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
